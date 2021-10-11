02bcf2593eed2e1eed71303677c38529673e7f59
Valdir Silva <valdirpiresba@gmail.com>
2021-10-11 17:04:24 -0300

    modificando a rota home da aplicacao

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,12 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
+/*
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
 ->name('home')
 ->middleware('verified');
 
+*/
 
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController')
 ->middleware('verified');;
