7793e34853d11b09599cca61820145e092933a19
Valdir Silva <valdirpiresba@gmail.com>
2021-10-11 19:29:25 -0300

    validando se a tarefa pertence ao usuario logado antes de habilitar a edicao

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -79,7 +79,13 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        return view('tarefa.edit', ['tarefa' => $tarefa]);
+        $user_id = auth()->user()->id;
+
+        if ( $tarefa->user_id == $user_id ) {
+            return view('tarefa.edit', ['tarefa' => $tarefa]);
+        }
+
+        return view('acesso-negado');
     }
 
     /**
@@ -91,8 +97,13 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        $tarefa->update($request->all());
+        $user_id = auth()->user()->id;
 
+        if ( !$tarefa->user_id == $user_id) {
+             return view('acesso-negado');
+        }
+
+        $tarefa->update($request->all());
         return redirect()->route('tarefa.show', ['tarefa' => $tarefa->id]);
     }
 
